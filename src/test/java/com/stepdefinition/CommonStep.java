65ffccc590e62f0e9016b629905f695223dfbc42
GITHUB <mahendran10000@gmail.com>
2023-03-02 09:56:20 +0530

    push

---
+++ src/test/java/com/stepdefinition/CommonStep.java
@@ -0,0 +1,22 @@
+package com.stepdefinition;
+
+import org.junit.Assert;
+
+import com.base.BaseClass;
+import com.pageManager.PageObjectManager;
+
+import io.cucumber.java.en.Then;
+
+public class CommonStep extends BaseClass {
+	
+	PageObjectManager pom = new PageObjectManager();
+
+	@Then("User should verify after login success message {string}")
+	public void userShouldVerifyAfterLoginSuccessMessage(String welcomeMsg) {
+
+		Assert.assertEquals("Verify the success message", welcomeMsg,
+				pom.getSearchHotelPage().getSuccessMsg1().getAttribute("value"));
+
+	}
+
+}
